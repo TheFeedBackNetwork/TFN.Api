b9f4da9a27159a937c446a310cd71561cf269cef
Lutando Ngqakaza <lutando@ngqakaza.com>
2017-04-19 17:24:10 +0200

    git init

--- src/TFN.Sts/Startup.cs
+++ src/TFN.Sts/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace TFN
+namespace TFN.Sts
 {
     public class Startup
     {
